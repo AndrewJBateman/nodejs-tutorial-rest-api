6596754761bdb1e4160fef3e8a2de50cacdf035d
Andrew Bateman <gomezbateman@yahoo.com>
2019-01-28 20:28:44 +0100

    socket.IO added for real-time comms

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ This is the code from section 24 of the Udemy Course [NodeJS - The Complete Guid
 It creates simple get and post routes/controllers to send and receive data, as well as header and status information. 
 
 A Postman API and a [Codepen REST API test app](https://codepen.io/AndrewJBateman/pen/dwbVaX?editors=0010) are used to send/receive data.
-[Multer](https://www.npmjs.com/package/multer) is the middleware used for uploading files.  
+[Multer](https://www.npmjs.com/package/multer) is the middleware used for uploading files. 
+[socket.io](https://socket.io/) version 2.0 is added for real-time event-based communications
 
 
 ## Notes
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const { validationResult } = require('express-validator/check');
 
+const io = require('../socket');
 const Post = require('../models/post');
 const User = require('../models/user');
 
@@ -13,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate('creator')
+      .sort({ createdAt: -1 })
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -44,7 +46,6 @@ exports.createPost = async (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-  let creator;
   const post = new Post({
     title: title,
     content: content,
@@ -52,14 +53,18 @@ exports.createPost = async (req, res, next) => {
     creator: req.userId
   });
   try {
-    await post.save()
+    await post.save();
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
+    io.getIO().emit('posts', {
+      action: 'create',
+      post: { ...post._doc, creator: { _id: req.userId, name: user.name } }
+    });
     res.status(201).json({
       message: 'Post created successfully!',
       post: post,
-      creator: { _id: creator._id, name: creator.name }
+      creator: { _id: user._id, name: user.name }
     });
   } catch (err) {
     if (!err.statusCode) {
@@ -71,7 +76,7 @@ exports.createPost = async (req, res, next) => {
 
 exports.getPost = async (req, res, next) => {
   const postId = req.params.postId;
-  const post = await Post.findById(postId)
+  const post = await Post.findById(postId);
   try {
     if (!post) {
       const error = new Error('Could not find post.');
@@ -107,13 +112,13 @@ exports.updatePost = async (req, res, next) => {
     throw error;
   }
   try {
-    const post = await Post.findById(postId)
+    const post = await Post.findById(postId).populate('creator');
     if (!post) {
       const error = new Error('Could not find post.');
       error.statusCode = 404;
       throw error;
     }
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized!');
       error.statusCode = 403;
       throw error;
@@ -125,19 +130,20 @@ exports.updatePost = async (req, res, next) => {
     post.imageUrl = imageUrl;
     post.content = content;
     const result = await post.save();
+    io.getIO().emit('posts', { action: 'update', post: result });
     res.status(200).json({ message: 'Post updated!', post: result });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
-  };
+  }
 };
 
 exports.deletePost = async (req, res, next) => {
   const postId = req.params.postId;
   try {
-    const post = await Post.findById(postId)
+    const post = await Post.findById(postId);
 
     if (!post) {
       const error = new Error('Could not find post.');
@@ -152,13 +158,13 @@ exports.deletePost = async (req, res, next) => {
     // Check logged in user
     clearImage(post.imageUrl);
     await Post.findByIdAndRemove(postId);
-    
+
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
-
+    io.getIO().emit('posts', { action: 'delete', post: postId });
     res.status(200).json({ message: 'Deleted post.' });
-  } catch(err) {
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
